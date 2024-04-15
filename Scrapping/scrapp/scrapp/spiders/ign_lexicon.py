42ed021c280b98b34efae5e7ac22dd6ee1102330
laurianePIERRE <pierrelauriane08@gmail.com>
2024-04-15 13:42:33 +0200

    parsing correctly v 2

--- Scrapping/scrapp/scrapp/spiders/ign_lexicon.py
+++ Scrapping/scrapp/scrapp/spiders/ign_lexicon.py
@@ -5,11 +5,45 @@ import re
 class IgnLexiconSpider(scrapy.Spider):
     name = "ign_lexicon"
     allowed_domains = ["www.ign.com"]
-    start_urls = ["https://www.ign.com/wikis/gaming-terms-lexicon/A/"]
+    start_urls = ["https://www.ign.com/wikis/gaming-terms-lexicon/A/",
+                  "https://www.ign.com/wikis/gaming-terms-lexicon/B/",
+                  "https://www.ign.com/wikis/gaming-terms-lexicon/C/",
+                  "https://www.ign.com/wikis/gaming-terms-lexicon/D/",
+                  "https://www.ign.com/wikis/gaming-terms-lexicon/E/",
+                  "https://www.ign.com/wikis/gaming-terms-lexicon/F/",
+                  "https://www.ign.com/wikis/gaming-terms-lexicon/G/",
+                  "https://www.ign.com/wikis/gaming-terms-lexicon/H/",
+                  "https://www.ign.com/wikis/gaming-terms-lexicon/I/",
+                  "https://www.ign.com/wikis/gaming-terms-lexicon/J/",
+                  "https://www.ign.com/wikis/gaming-terms-lexicon/K/",
+                  "https://www.ign.com/wikis/gaming-terms-lexicon/L/",
+                  "https://www.ign.com/wikis/gaming-terms-lexicon/M/",
+                  "https://www.ign.com/wikis/gaming-terms-lexicon/N/",
+                  "https://www.ign.com/wikis/gaming-terms-lexicon/O/",
+                  "https://www.ign.com/wikis/gaming-terms-lexicon/P/",
+                  "https://www.ign.com/wikis/gaming-terms-lexicon/Q/",
+                  "https://www.ign.com/wikis/gaming-terms-lexicon/R/",
+                  "https://www.ign.com/wikis/gaming-terms-lexicon/S/",
+                  "https://www.ign.com/wikis/gaming-terms-lexicon/T/",
+                  "https://www.ign.com/wikis/gaming-terms-lexicon/U/",
+                  "https://www.ign.com/wikis/gaming-terms-lexicon/V/",
+                  "https://www.ign.com/wikis/gaming-terms-lexicon/W/",
+                  "https://www.ign.com/wikis/gaming-terms-lexicon/X/",
+                  "https://www.ign.com/wikis/gaming-terms-lexicon/Y/",
+                  "https://www.ign.com/wikis/gaming-terms-lexicon/Z/",]
 
+    # variable globale pour suivre le nombre de termes scrappés
+
+    total_terms_scrapped = 0
+
+
+    def __init__(self,*args,**kwargs):
+        super(IgnLexiconSpider, self).__init__(*args,**kwargs)
+        # Reinizalize viable each excute spider
+        self.total_terms_scrapped =0
     def parse(self, response):
         sections = response.xpath("//section[@class='content']")
-
+        print("total : ",self.total_terms_scrapped)
         # sans se soucier des balises <b>
 
         words = sections.xpath("//span[@class='mw-headline']/text()").getall()
@@ -29,6 +63,7 @@ class IgnLexiconSpider(scrapy.Spider):
 
         definitions[0] = "".join(concatenated_texts)
         final_definitions = []
+
         for definition in definitions:
             sel= Selector(text=definition)
             clean_text = sel.xpath("string()").get()
@@ -36,15 +71,15 @@ class IgnLexiconSpider(scrapy.Spider):
             final_definitions.append(clean_text)
 
 
-     #   print("--------------------------------------------------------------------------------")
-      #  print(final_definitions[0:4])
-       # print(len(final_definitions))
-        #print(len(words))
-
-        print (final_definitions)
+        print (" le documents se compose de ",len(words)," mots, et donc ",len(final_definitions), " definitions")
         for word,definition in  zip(words,final_definitions):
+            # increments
+            self.total_terms_scrapped +=1
             yield {
                 "EN" : word,
                 "definition":definition
             }
-
+        print("______________________________________________________________________________________________________")
+        print("total : ", self.total_terms_scrapped)
+    def Close (self, reason):
+        print("Nombre total de termes scrappés :",self.total_terms_scrapped)
